24bee03e9f18adf69bde51dbdb1fbf88e39e7aa6
satria97 <satria.code97@gmail.com>
2023-01-23 21:35:13 +0700

    collaboration implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,11 @@
-const mapDBToModel = ({ id, title, tags, body, created_at, updated_at }) => ({
+const mapDBToModel = ({ id, title, tags, body, created_at, updated_at, username }) => ({
 	id,
 	title,
 	tags,
 	body,
 	createdAt: created_at,
 	updatedAt: updated_at,
+	username,
 });
 
 module.exports = { mapDBToModel };
